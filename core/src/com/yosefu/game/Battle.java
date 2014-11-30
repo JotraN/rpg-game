e3cd21ddbc2bacf8c58cdfe13e10db72934177f6
JoTran <josephthientran@gmail.com>
2014-11-29 23:20:19 -0500

    Made Stats members public since each instance of it is private to each object. Added attack animation to battles.

--- core/src/com/yosefu/game/Battle.java
+++ core/src/com/yosefu/game/Battle.java
@@ -14,16 +14,18 @@ public class Battle implements Screen {
     private final Screen screen;
     private final Player player;
     private final Enemy enemy;
-    //    private Array<Enemy> enemies;
     private ShapeRenderer shapeRenderer;
     private OrthographicCamera camera;
     private Texture bg;
     private Color textColor = new Color(0.2f, 0.3f, 0.2f, 1.0f);
     private Color highlightColor = new Color(0.6f, 0.7f, 0.6f, 1.0f);
     private String[] textOptions = {
-            "attack", "breathe", "talk", "run"
+            "attack", "defend", "talk", "run"
     };
-    private int current = 0;
+    private int selection = 0;
+    private boolean attacking = false, playerAttacking = false;
+    private int playerX = 300, playerY = 340, enemyX = 900, enemyY = 340;
+    private int velY = 5;
 
     public Battle(Player player, Enemy enemy, Yosefu game, Screen screen) {
         this.player = player;
@@ -38,13 +40,17 @@ public class Battle implements Screen {
 
     @Override
     public void render(float delta) {
-        if (player.getStats().getHealth() < 0) return;
-        if (enemy.getStats().getHealth() < 0) {
+        // TODO Game over.
+        if (player.getStats().health < 0) return;
+        if (enemy.getStats().health < 0) {
             enemy.kill();
             game.setScreen(new TransitionScreen(game, screen));
         }
         draw();
-        processEvents();
+        if(attacking)
+            animateAttack();
+        else
+            processEvents();
     }
 
     private void draw() {
@@ -56,18 +62,17 @@ public class Battle implements Screen {
         game.font.setColor(highlightColor);
         game.font.setScale(1);
         game.batch.draw(bg, 0, 0);
-        game.batch.draw(player.getTexture(), 300, 340);
-        game.font.draw(game.batch, "hp: " + player.getStats().getHealth(), 300, 340 + player.height + 25);
-        game.batch.draw(enemy.getTexture(), 900, 340);
-        game.font.draw(game.batch, "hp: " + enemy.getStats().getHealth(), 900, 340 + enemy.height + 25);
+        game.batch.draw(player.getTexture(), playerX, playerY);
+        game.font.draw(game.batch, "hp: " + player.getStats().health, 300, 340 + player.height + 25);
+        game.batch.draw(enemy.getTexture(), enemyX, enemyY);
+        game.font.draw(game.batch, "hp: " + enemy.getStats().health, 900, 340 + enemy.height + 25);
         game.batch.end();
 
-        float x = 50;
-        float y = 40;
-        float width = 150;
-        float height = 85;
+        int x = 50;
+        int y = 40;
+        int width = 150;
+        int height = 85;
         int border = 4;
-
         shapeRenderer.setProjectionMatrix(camera.combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         // Draw player box.
@@ -83,35 +88,59 @@ public class Battle implements Screen {
         y += height;
         for (int i = 0; i < textOptions.length; i++) {
             game.font.setColor(textColor);
-            if (current == i)
+            if (selection == i)
                 game.font.setColor(highlightColor);
             game.font.draw(game.batch, textOptions[i], x + padding, y - padding);
             y -= padding;
         }
-//        game.font.draw(game.batch, "pp: " + player.getStats().getPp(), xLoc, yLoc * 2);
         game.batch.end();
     }
 
     private void processEvents() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            if (current == 0) return;
-            current--;
+            if (selection == 0) return;
+            selection--;
         } else if (Gdx.input.isKeyJustPressed(Input.Keys.S) || Gdx.input.isKeyJustPressed((Input.Keys.DOWN))) {
-            if (current == textOptions.length - 1) return;
-            current++;
+            if (selection == textOptions.length - 1) return;
+            selection++;
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.O))
             act();
     }
 
     private void act() {
-        switch (current) {
+        switch (selection) {
             case 0:
-                enemy.getStats().setHealth(enemy.getStats().getHealth() - player.getStats().getAtk());
-                player.getStats().setHealth(player.getStats().getHealth() - enemy.getStats().getAtk());
+                attacking = true;
+                playerAttacking = true;
                 break;
             default:
                 break;
+        }
+    }
+
+    private void animateAttack(){
+        if(playerAttacking) {
+            playerY += velY;
+            if (playerY > 360)
+                velY = -velY;
+            if (playerY < 340) {
+                playerY = 340;
+                playerAttacking = false;
+                velY = -velY;
+                enemy.getStats().health -= player.getStats().atk;
+            }
+        } else{
+            // Animate enemy attack.
+            enemyY += velY;
+            if (enemyY > 360)
+                velY = -velY;
+            if (enemyY < 340) {
+                enemyY = 340;
+                attacking = false;
+                velY = -velY;
+                player.getStats().health -= enemy.getStats().atk;
+            }
         }
     }
 
--- core/src/com/yosefu/game/Stats.java
+++ core/src/com/yosefu/game/Stats.java
@@ -1,38 +1,14 @@
 package com.yosefu.game;
 
 public class Stats {
-    private float health = 10;
-    private float pp = 10;
-    private float atk = 10;
+    public float health = 10;
+    public float pp = 10;
+    public float atk = 10;
 
     public Stats(float health, float pp, float atk) {
         this.health = health;
         this.pp = pp;
         this.atk = atk;
-    }
-
-    public void setHealth(float health) {
-        this.health = health;
-    }
-
-    public void setPp(float pp) {
-        this.pp = pp;
-    }
-
-    public void setAtk(float atk) {
-        this.atk = atk;
-    }
-
-    public float getHealth() {
-        return health;
-    }
-
-    public float getPp() {
-        return pp;
-    }
-
-    public float getAtk() {
-        return atk;
     }
 }
 
